596bbb68eebe0511448f7891459d4e7c39ab5743
ErnestKojo <kojokobi@outlook.com>
2023-04-25 10:52:13 +0000

    updated required project files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="kojokobi/app"
+imagename="app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $imagename --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/$imagename 8000:80
+
 
